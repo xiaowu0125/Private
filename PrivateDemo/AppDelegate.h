e7e96e88443d95d3d73f059d362d8c145b0112a2
fansen <18037860536@163.com>
2020-12-31 15:58:06 +0800

    Initial Commit

---
+++ PrivateDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PrivateDemo
+//
+//  Created by fansen on 2020/12/31.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PrivateDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  PrivateDemo
+//
+//  Created by fansen on 2020/12/31.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
